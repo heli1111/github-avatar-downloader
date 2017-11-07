442c126dd79fa7f30bf98496f472bf790a54a6d6
Jessica <hepu24@gmail.com>
2017-11-07 03:22:30 +0000

    making avatar requests, adding headers, adding authentication

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const secrets = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -7,6 +8,17 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 //   repoOwner, repoName - github repository identification
 function getRepoContributors(repoOwner, repoName, cb) {
   // curl -u <username>:<token> https://api.github.com/repos/jquery/jquery/contributors
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'hepuliu',
+      'Authorization': 'token ' + secrets.GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });  
 }
 
 // calling getRepoContributors with hard-coded values
