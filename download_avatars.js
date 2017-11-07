f00637592b4946dec6611d104a661a97922bb5e5
Jessica <hepu24@gmail.com>
2017-11-07 02:54:42 +0000

    calling getRepoContributors with hard-coded arguments

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,18 @@
 const request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+// getRepoContributors - fetch list of contributors
+//   cb - callback function to handle results - cb(err, result)
+//   repoOwner, repoName - github repository identification
+function getRepoContributors(repoOwner, repoName, cb) {
+  // curl -u <username>:<token> https://api.github.com/repos/jquery/jquery/contributors
+}
+
+// calling getRepoContributors with hard-coded values
+// callback function passes error first and response
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
